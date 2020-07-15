1ce1f53d2cbbdb23d9222a14ba4fe8e03f7da923
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-07-14 21:40:28 -0700

    1
    1

---
+++ dealershipProject/dealershipLot.js
@@ -0,0 +1,11 @@
+var inventory = {
+    type:"Ford",
+    model:"Mustang", 
+    amount:10,
+};
+
+function carRequest(make, model, amount) {
+    this.make = "Porshe";
+    this.model = "GT3RS";
+    this.amount = 2
+}
