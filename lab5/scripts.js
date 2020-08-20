eb30124ae8065f5227b02ee41e903163a6324c74
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-08-20 15:46:20 -0700

    1

--- lab5/scripts.js
+++ lab5/scripts.js
@@ -1,23 +1,26 @@
-// STORING DATA
 
-//stores data 
+//function 
 $("#infoBtn").click(function(){    
 
+//gathers input
   let personInfo = {
   name: document.getElementById('name').value,
   gpa: document.getElementById('gpa').value,
   age: document.getElementById('age').value,
   program: document.getElementById('prog').value,
-}      
+}  
+
+//stores data     
     var pInfoJSON = JSON.stringify(personInfo);
 
     localStorage.setItem("pInfoJSON", pInfoJSON);  
 
+//gathers stored data
     var pInfoText = localStorage.getItem("pInfoJSON");
 
     var pInfoObj = JSON.parse(pInfoText);
 
-
+//displays data
   $("#pName").html(pInfoObj.name);
   $("#pGpa").html(pInfoObj.gpa);
   $("#pAge").html(pInfoObj.age);
